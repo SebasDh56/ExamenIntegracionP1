6613a86d73da067fe73ce440303b589d27c2c0c1
Amable Sebastian Quishpe <105821062+SebasDh56@users.noreply.github.com>
2026-04-23 20:25:58 -0500

    Examen Integracion P1

---
+++ src/main/java/com/saludvital/FileIntegrationApplication.java
@@ -0,0 +1,11 @@
+package com.saludvital;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileIntegrationApplication.class, args);
+    }
+}
